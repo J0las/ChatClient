29619de05dd57a7485e2cf55c5363d05a65d7c7b
Felix-Markus <67581887+Felix-Markus@users.noreply.github.com>
2020-07-07 10:35:19 +0200

    Create SendMessage.java

---
+++ src/chatclient/lib/SendMessage.java
@@ -0,0 +1,14 @@
+package chatclient.lib;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SendMessage implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //Launcher.selectedConnection;
+        
+    }
+
+}
